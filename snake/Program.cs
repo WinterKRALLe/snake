610b168ce0938566bee5f555451ad5ff248a3511
Jindřich Caletka <j_caletka@ad.utb.cz>
2025-03-18 11:14:43 +0100

    inheritance

--- snake/Program.cs
+++ snake/Program.cs
@@ -8,13 +8,42 @@ public enum Direction
     Right
 }
 
+// Základní třída pro všechny herní prvky
 public class Pixel
 {
     public int X { get; set; }
     public int Y { get; set; }
-    public ConsoleColor Color { get; init; }
+    public ConsoleColor Color { get; protected init; }
 }
 
+// Hlava hada
+public class SnakeHead : Pixel
+{
+    public SnakeHead()
+    {
+        Color = ConsoleColor.Red; // Výchozí barva hlavy
+    }
+}
+
+// Tělo hada
+public class SnakeBody : Pixel
+{
+    public SnakeBody()
+    {
+        Color = ConsoleColor.Green; // Výchozí barva těla
+    }
+}
+
+// Ovoce (Berry)
+public class Berry : Pixel
+{
+    public Berry()
+    {
+        Color = ConsoleColor.Cyan; // Výchozí barva ovoce
+    }
+}
+
+// Hlavní třída hry
 public class SnakeGame
 {
     private const int ScreenWidth = 32;
@@ -22,22 +51,23 @@ public class SnakeGame
     private readonly Random _random = new();
     private int _score = 5;
     private bool _gameOver;
-    private readonly Pixel _head;
-    private readonly List<Pixel> _body = [];
-    private readonly Pixel _berry;
+    private readonly SnakeHead _head;
+    private readonly List<SnakeBody> _body = [];
+    private readonly Berry _berry;
     private Direction _movement = Direction.Right;
     private DateTime _lastUpdate = DateTime.Now;
     private bool _buttonPressed;
 
+    // Konstruktor hry
     public SnakeGame()
     {
         Console.WindowHeight = ScreenHeight;
         Console.WindowWidth = ScreenWidth;
-        _head = new Pixel { X = ScreenWidth / 2, Y = ScreenHeight / 2, Color = ConsoleColor.Red };
-        _berry = new Pixel
-            { X = _random.Next(1, ScreenWidth - 1), Y = _random.Next(1, ScreenHeight - 1), Color = ConsoleColor.Cyan };
+        _head = new SnakeHead { X = ScreenWidth / 2, Y = ScreenHeight / 2 };
+        _berry = new Berry { X = _random.Next(1, ScreenWidth - 1), Y = _random.Next(1, ScreenHeight - 1) };
     }
 
+    // Hlavní herní smyčka
     public void Run()
     {
         while (!_gameOver)
@@ -64,6 +94,7 @@ public class SnakeGame
         ShowGameOver();
     }
 
+    // Vykreslení hranic
     private static void DrawBorders()
     {
         for (var i = 0; i < ScreenWidth; i++)
@@ -83,11 +114,13 @@ public class SnakeGame
         }
     }
 
+    // Vykreslení herního prvku (■)
     private static void DrawFruit()
     {
         Console.Write("■");
     }
 
+    // Vykreslení ovoce
     private void DrawBerry()
     {
         Console.ForegroundColor = _berry.Color;
@@ -95,24 +128,34 @@ public class SnakeGame
         DrawFruit();
     }
 
+    // Kontrola kolizí
     private void HandleCollisions()
     {
+        // Kolize s hranicemi
         if (_head.X == 0 || _head.X == ScreenWidth - 1 || _head.Y == 0 || _head.Y == ScreenHeight - 1)
         {
             _gameOver = true;
         }
 
-        foreach (var unused in _body.Where(part => part.X == _head.X && part.Y == _head.Y))
+        // Kolize s tělem
+        foreach (var part in _body)
         {
-            _gameOver = true;
+            if (part.X == _head.X && part.Y == _head.Y)
+            {
+                _gameOver = true;
+            }
         }
 
-        if (_head.X != _berry.X || _head.Y != _berry.Y) return;
-        _score++;
-        _berry.X = _random.Next(1, ScreenWidth - 1);
-        _berry.Y = _random.Next(1, ScreenHeight - 1);
+        // Kolize s ovocem
+        if (_head.X == _berry.X && _head.Y == _berry.Y)
+        {
+            _score++;
+            _berry.X = _random.Next(1, ScreenWidth - 1);
+            _berry.Y = _random.Next(1, ScreenHeight - 1);
+        }
     }
 
+    // Vykreslení hada
     private void DrawSnake()
     {
         Console.ForegroundColor = _head.Color;
@@ -121,15 +164,16 @@ public class SnakeGame
 
         foreach (var part in _body)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = part.Color;
             Console.SetCursorPosition(part.X, part.Y);
             DrawFruit();
         }
     }
 
+    // Aktualizace pozice hada
     private void UpdateSnakePosition()
     {
-        _body.Add(new Pixel { X = _head.X, Y = _head.Y, Color = ConsoleColor.Green });
+        _body.Add(new SnakeBody { X = _head.X, Y = _head.Y });
 
         switch (_movement)
         {
@@ -155,6 +199,7 @@ public class SnakeGame
         }
     }
 
+    // Zpracování vstupu od uživatele
     private void HandleInput()
     {
         while (true)
@@ -169,18 +214,20 @@ public class SnakeGame
                 ConsoleKey.DownArrow when _movement != Direction.Up && !_buttonPressed => Direction.Down,
                 ConsoleKey.LeftArrow when _movement != Direction.Right && !_buttonPressed => Direction.Left,
                 ConsoleKey.RightArrow when _movement != Direction.Left && !_buttonPressed => Direction.Right,
-                _ => throw new ArgumentOutOfRangeException()
+                _ => _movement // Zachová původní směr při neplatném vstupu
             };
 
             _buttonPressed = true;
         }
     }
 
+    // Čekání na další snímek
     private static void WaitForNextFrame()
     {
         Thread.Sleep(100);
     }
 
+    // Zobrazení konce hry
     private void ShowGameOver()
     {
         Console.SetCursorPosition(ScreenWidth / 5, ScreenHeight / 2);
@@ -189,6 +236,7 @@ public class SnakeGame
     }
 }
 
+// Spouštěcí třída
 internal abstract class Program
 {
     private static void Main()
